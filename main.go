f7bd7cc9fa43680a38413cabe853880be1405011
Shpak Roman <romq1@mail.ru>
2019-10-01 00:57:25 +0300

    make it with scoping

--- main.go
+++ main.go
@@ -25,20 +25,14 @@ func main() {
 }
 
 func makeSeq(count int) []string {
-  arr := []string{"1"}
-
   i := 0
+  addNextElement, result := elementGenerator()
   for i != count {
-    letter := arr[len(arr) - 1]
-    arrayLetters := strings.Split(letter, "")
-
-    nextNumber := makeNextSeqNumber(arrayLetters)
-    arr = append(arr, nextNumber)
-
+    addNextElement()
     i = i + 1
   }
 
-  return arr
+  return result()
 }
 
 type computedElem struct {
@@ -46,30 +40,35 @@ type computedElem struct {
   elem string
 }
 
-// ['1', '1', '1', '2', '2'] => '3122'
-func makeNextSeqNumber(arrayStr []string) string {
-  var arrayOfElements []computedElem
+func elementGenerator() (func(), func() []string) {
+  resultArray := make([]string, 1)
+  resultArray[0] = "1"
 
-  for index, elem := range arrayStr {
-    if (index == 0) {
-      arrayOfElements = append(arrayOfElements, computedElem{1, elem})
-      continue
+  addNextElement := func() {
+    var arrayOfElements []computedElem
+    letter := resultArray[len(resultArray) - 1]
+    arrayStr := strings.Split(letter, "")
+
+    for index, elem := range arrayStr {
+      if (index != 0 && elem == arrayStr[index - 1]) {
+        arrayOfElements[len(arrayOfElements) - 1].count++
+      } else {
+        arrayOfElements = append(arrayOfElements, computedElem{1, elem})
+      }
     }
 
-    if (elem == arrayStr[index - 1]) {
-      arrayOfElements[len(arrayOfElements) - 1].count++
-    } else {
-      arrayOfElements = append(arrayOfElements, computedElem{1, elem})
+    interArray := []string{}
+    for _, computedElem := range arrayOfElements {
+      countStr := strconv.Itoa(computedElem.count)
+      strArray := []string{countStr, computedElem.elem}
+      interArray = append(interArray, strings.Join(strArray, ""))
     }
+    resultArray = append(resultArray, strings.Join(interArray, ""))
   }
 
-  interArray := []string{}
-  for _, computedElem := range arrayOfElements {
-    countStr := strconv.Itoa(computedElem.count)
-    strArray := []string{countStr, computedElem.elem}
-    interArray = append(interArray, strings.Join(strArray, ""))
+  result := func() []string {
+    return resultArray
   }
 
-  return strings.Join(interArray, "")
+  return addNextElement, result
 }
-
